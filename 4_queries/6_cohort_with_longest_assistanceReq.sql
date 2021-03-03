8a517421e54ca73b17011f74a659f6368b1e7ae4
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-03-02 18:27:58 -0700

    query for cohort with longest assistance requests

---
+++ 4_queries/6_cohort_with_longest_assistanceReq.sql
@@ -0,0 +1,9 @@
+SELECT MAX(average_assistance)
+FROM (
+  SELECT cohorts.name, AVG(completed_at - started_at) as average_assistance
+  FROM students
+  JOIN assistance_requests ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  GROUP BY cohorts.name
+  ORDER BY AVG(completed_at - started_at)
+) newTable;
