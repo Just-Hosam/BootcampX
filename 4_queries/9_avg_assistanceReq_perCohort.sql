7801643c2dabebbe6f156c2d665f7ecb2a2f4d0e
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-03-02 18:28:50 -0700

    query for avg assistance requests per cohort

---
+++ 4_queries/9_avg_assistanceReq_perCohort.sql
@@ -0,0 +1,9 @@
+SELECT AVG(total_duration)
+FROM (
+  SELECT cohorts.name, SUM(completed_at - started_at) as total_duration
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  JOIN assistance_requests ON students.id = student_id
+  GROUP BY cohorts.name
+  ORDER BY total_duration
+) AS newTable;
