bd069b8da61477d6fc74dbc2e5c7aa96e0db3549
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-03-03 20:56:29 -0700

    query for teachers in 1 cohort

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,3 +6,19 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+JOIN assistance_requests ON students.id = student_id
+JOIN teachers ON teachers.id = teacher_id
+WHERE cohorts.name = '${process.argv[2]}'
+ORDER BY teachers.name;
+`)
+.then(res => {
+  console.log('connected');
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
